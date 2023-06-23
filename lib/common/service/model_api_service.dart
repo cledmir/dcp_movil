4d067ba13b94fbae115ed61051204af6d43a8c34
Cledmir Gerardo Tejada Silva <56932765+cledmir@users.noreply.github.com>
2023-06-22 22:15:08 -0500

    Revert "update"
    This reverts commit ec7919ab8e14e3a81d8141570b4fe63e50b378c0.

--- lib/common/service/model_api_service.dart
+++ lib/common/service/model_api_service.dart
@@ -2,12 +2,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl =
-      'https://despliegue-tp2-docker.azurewebsites.net';
+  static const String baseUrl = 'https://dcp-api-docker.azurewebsites.net';
 
   Future<dynamic> predictImage(String imageUrl) async {
-    final url = Uri.parse(
-        '$baseUrl/predict?image_link=${Uri.encodeComponent(imageUrl)}');
+    final url =
+        Uri.parse('$baseUrl/predict?url=${Uri.encodeComponent(imageUrl)}');
     print("el url a enviar al api es $url");
     final response = await http.post(url);
     if (response.statusCode == 200) {
